5051507a8a5a4f7042df7540b9f41f5ab6c7b161
Laura Lucas <webdev.lauralucas@gmail.com>
2021-07-23 11:40:26 +0200

    modification du lien d'ajout de chart.js

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -23,4 +23,38 @@ table_to_array("table1");
 console.log(data);
 
 let newDiv = document.createElement("div");
+let newCanv = document.createElement("canvas");
+newCanv.setAttribute("id", "Canvas1");
+newDiv.appendChild(newCanv);
 table1ParentDiv.insertBefore(newDiv, table1);
+
+var ctx = document.getElementById('Canvas1').getContext('2d');
+
+const config = {
+    type: 'line',
+    data,
+    options: {}
+  };
+
+  const labels = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+  ];
+  const data = {
+    labels: labels,
+    datasets: [{
+      label: 'My First dataset',
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: [0, 10, 5, 2, 20, 30, 45],
+    }]
+  };
+
+  var myChart = new Chart(
+    ctx,
+    config
+  );
