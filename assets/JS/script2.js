f63147bdb4d1998dd939d73dbcbebbfb6bcb6b03
Elsa Magalhaes <elsa.magalhaes@gmail.com>
2021-07-26 16:29:38 +0200

    api

--- assets/JS/script2.js
+++ assets/JS/script2.js
@@ -1,42 +1,58 @@
 let chartDiv=document.createElement("div");
+chartDiv.setAttribute("id","chart");
+let Canvas = document.createElement("canvas");
+chartDiv.setAttribute('width', '500px');
+chartDiv.setAttribute('height', '500px');
+Canvas.setAttribute("id", "CanvasChart");
+chartDiv.appendChild(Canvas);
 let chartParentDiv = document.getElementById("bodyContent").parentNode;
 chartParentDiv.insertBefore(chartDiv, bodyContent);
-//let svg = dimple.newSvg(chartDiv, 590, 400);
+let ctx1 = document.getElementById("CanvasChart").getContext("2d");
+let labels1=[];
+let dataPoints1=[];
 
-window.onload = function() {
-	let dataPoints = [];
 	fetch("https://canvasjs.com/services/data/datapoints.php")
 	.then(response=>response.json())	
     .then(data=>{
         data.forEach(item=>{
-			dataPoints.push({x: item[0], y: parseInt(item[1])});
-            let chart = new dimple.chart(svg,dataPoints);
-            chart.setBounds(80, 30, 480, 330)
-            chart.addMeasureAxis("x");
-            chart.addCategoryAxis("y");
-            chart.addSeries(" ", dimple.plot.bar);
-            //chart.addLegend(60, 10, 510, 20, "right");
-            console.log(chart);
-            chart.draw();
+			labels1.push(item[0]);
+			dataPoints1.push(item[1]);
+			//console.log();
 		});
-        
-        
-        console.log(chart);
-		//chart.render();
-		updateChart();
+		console.log(dataPoints1);
 	});
-	function updateChart() {
-	fetch("https://canvasjs.com/services/data/datapoints.php?xstart=" + (dataPoints.length + 1) + "&ystart=" + (dataPoints[dataPoints.length - 1].y) + "&length=1&type=json")
-    .then (response=>response.json())
-	.then(data=>
-        	data.forEach(item=> {
-			dataPoints.push({
-			x: parseInt(item[0]),
-			y: parseInt(item[1])
-			});
-		}));
-		//chart.render();
-		setTimeout(function(){updateChart()}, 1000);
-	};
 	
-}
+	
+	/*function updateChart() {
+		fetch("https://canvasjs.com/services/data/datapoints.php?xstart=" + (labels1.length + 1) + "&ystart=" + ([dataPoints.length - 1].y) + "&length=1&type=json")
+		.then (response=>response.json())
+		.then(data=>
+				data.forEach(item=> {
+					labels1.push(item[0])
+					dataPoints.push(item[1]),
+				});
+			}));
+			
+			setTimeout(function(){updateChart()}, 1000);
+		};*/
+		
+		const data1= {
+		labels: labels1,
+		datasets: [
+			{
+			label: 'Dataset 1',
+			data: dataPoints1,
+			fill: false,
+			borderColor: 'rgb(75, 192, 192)',
+			tension: 0.1
+			}]
+		  };
+			  
+const config1 = {
+	type: 'line',
+	data: data1,
+  };
+var myChart1 = new Chart(ctx1,config1);
+  
+
+  
